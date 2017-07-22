f8cba219bcf19a8b8460d4d3f731f4b4ebd254d8
Jesse Nicholson <TechnikEmpire@users.noreply.github.com>
2017-07-22 02:07:08 -0400

    Rename core csproject
    Rename core csproject to CitadelGUI project. Initial creation of CitadelService project.

---
+++ CitadelService/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CitadelService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
