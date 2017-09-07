419cdb2316dd28e04bf4834214b813c0d59f37b3
Jesse Nicholson <TechnikEmpire@users.noreply.github.com>
2017-09-06 21:03:09 -0400

    Fix post install commands
    Fixes #51
    
    Version bump

--- CitadelGUI/Te/Citadel/PostInstallCommand.cs
+++ CitadelGUI/Te/Citadel/PostInstallCommand.cs
@@ -24,7 +24,7 @@ namespace Te.Citadel
     public partial class PostInstallCommand : System.Configuration.Install.Installer
     {
         public PostInstallCommand()
-        {
+        {   
             InitializeComponent();
         }
 
