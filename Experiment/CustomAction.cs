405e1ff1266f7c6652fb34a6fbec88cbc1ccafc1
Kent Friesen <kfreezen@gmail.com>
2018-01-23 12:54:22 -0500

    Merging wix installer stuff.

---
+++ Experiment/CustomAction.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Deployment.WindowsInstaller;
+
+namespace Experiment
+{
+    public class CustomActions
+    {
+        [CustomAction]
+        public static ActionResult CustomAction1(Session session)
+        {
+            session.Log("Begin CustomAction1");
+
+            return ActionResult.Success;
+        }
+    }
+}
