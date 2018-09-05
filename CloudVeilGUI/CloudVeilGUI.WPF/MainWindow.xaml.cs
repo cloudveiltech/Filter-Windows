3f9b109b640817bd8a84452256e5e6bd239be883
Kent Friesen <kfreezen@gmail.com>
2018-09-05 08:43:57 -0400

    Adding a WPF head project for CloudVeilGUI.

---
+++ CloudVeilGUI/CloudVeilGUI.WPF/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Forms.Platform.WPF;
+using Xamarin.Forms;
+
+namespace CloudVeilGUI.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : FormsApplicationPage
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Forms.Init();
+
+            LoadApplication(new CloudVeilGUI.App());
+        }
+    }
+}
