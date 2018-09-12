d7721c25764f13323a636591c07b76d57e217a32
Kent Friesen <kfreezen@gmail.com>
2018-09-12 08:10:35 -0400

    Wired up basic login page.

--- CloudVeilGUI/CloudVeilGUI/Views/LoginPage.xaml.cs
+++ CloudVeilGUI/CloudVeilGUI/Views/LoginPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CloudVeilGUI.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,15 @@ namespace CloudVeilGUI.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class LoginPage : ContentPage
 	{
+        LoginViewModel viewModel;
+
 		public LoginPage ()
 		{
 			InitializeComponent ();
+
+            viewModel = new LoginViewModel();
+
+            BindingContext = viewModel;
 		}
-	}
+    }
 }
