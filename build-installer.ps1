1bf5efad73bbe1c4246d7fc17ceafedd154d015c
Kent Friesen <kfreezen@gmail.com>
2019-06-20 09:27:42 -0400

    Working on signing individual CV4W components.

--- build-installer.ps1
+++ build-installer.ps1
@@ -19,12 +19,58 @@ Function Find-MsBuild([int] $MaxVersion = 2017)
     throw "Yikes - Unable to find msbuild"
 }
 
-<# Finish this tomorrow
+<# Finish this tomorrow #>
 Function Find-SignTool()
 {
     $winsdkKey = Get-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SDKs\Windows\v8.0"
-    If 
-} #>
+    If (!$winsdkKey -or !$winsdkKey.InstallationFolder) {
+        $winsdkKey = Get-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Kits\Installed Roots"
+
+        If (!$winsdkKey) { return '' }
+        If (!$winsdkKey.KitsRoot10) { return '' }
+
+        $winsdk = $winsdkKey.KitsRoot10
+    } Else {
+        If(!$winsdkKey.InstallationFolder) { return '' }
+        $winsdk = $winsdkKey.InstallationFolder
+    }
+
+    $win10SDKKey = Get-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SDKs\Windows\v10.0"
+
+    If(!$win10SDKKey) {
+        return ''
+    }
+
+    $win10SDKBinPath = $win10SDKKey.InstallationFolder + "bin\"
+    $win10SDKVersion = $win10SDKKey.ProductVersion + ".0\"
+    $win10SDKVersionBinPath = $win10SDKBinPath + $win10SDKVersion
+
+    $signtool0 = $win10SDKBinPath + "signtool.exe"
+    $signtool1 = $win10SDKVersionBinPath + "signtool.exe"
+
+    if(Test-Path $signtool0) {
+        return $signtool0
+    }
+
+    if(Test-Path $signtool1) {
+        return $signtool1
+    }
+
+    return ''
+}
+
+Function Does-SignTool-Have-Certificate($SignToolPath) {
+    echo "Testing Signature" > __signtool-certificate-tester__.txt
+    & $SignToolPath sign /fd SHA512 /tr http://timestamp.comodoca.com /a __signtool-certificate-tester__.txt
+    $retVal = $LastExitCode
+    del __signtool-certificate-tester__.txt
+
+    If($retVal -ne 0) {
+        return $false
+    } Else {
+        return $true
+    }
+}
 
 $configuration = "Release"
 if($Env:CONFIGURATION -eq "Debug") {
@@ -32,6 +78,8 @@ if($Env:CONFIGURATION -eq "Debug") {
 }
 
 $msbuildPath = Find-MsBuild
+$signtoolPath = Find-SignTool
+$useSigntool = Does-SignTool-Have-Certificate $signtoolPath
 
 $currentLocation = Get-Location
 
@@ -84,21 +132,21 @@ $output64 = Join-Path $currentLocation "Installers\SetupProjects\$configuration\
 $bundle64 = Join-Path $currentLocation "CloudVeilInstaller\bin\$configuration\CloudVeilInstaller-x64.exe"
 
 <# Sign executable files x64 #>
-<#
-signtool sign /fd SHA512 /tr http://timestamp.comodoca.com /a CitadelGUI\bin\$configuration x64\CloudVeil.exe
-signtool sign /fd SHA512 /tr http://timestamp.comodoca.com /a CitadelGUI\bin\$configuration x64\FilterServiceProvider.exe
-signtool sign /fd SHA512 /tr http://timestamp.comodoca.com /a CitadelGUI\bin\$configuration x64\FilterAgent.Windows.exe
-#>
+If($useSigntool) {
+    & $signtoolPath sign /fd SHA512 /tr http://timestamp.comodoca.com /a CitadelGUI\bin\$configuration x64\CloudVeil.exe
+    & $signtoolPath sign /fd SHA512 /tr http://timestamp.comodoca.com /a CitadelGUI\bin\$configuration x64\FilterServiceProvider.exe
+    & $signtoolPath sign /fd SHA512 /tr http://timestamp.comodoca.com /a CitadelGUI\bin\$configuration x64\FilterAgent.Windows.exe
+}
 
 & $msbuildPath /p:Configuration=$configuration /p:SolutionDir=$currentLocation $payload64 /t:Clean,Build
 & $msbuildPath /p:Configuration=$configuration /p:SolutionDir=$currentLocation $setup64 /t:Clean,Build,SignMsi
 
 <# Sign executable files x86 #>
-<#
-signtool sign /fd SHA512 /tr http://timestamp.comodoca.com /a CitadelGUI\bin\$configuration x86\CloudVeil.exe
-signtool sign /fd SHA512 /tr http://timestamp.comodoca.com /a CitadelGUI\bin\$configuration x86\FilterServiceProvider.exe
-signtool sign /fd SHA512 /tr http://timestamp.comodoca.com /a CitadelGUI\bin\$configuration x86\FilterAgent.Windows.exe
-#>
+if($useSigntool) {
+    & $signtoolPath sign /fd SHA512 /tr http://timestamp.comodoca.com /a CitadelGUI\bin\$configuration x86\CloudVeil.exe
+    & $signtoolPath sign /fd SHA512 /tr http://timestamp.comodoca.com /a CitadelGUI\bin\$configuration x86\FilterServiceProvider.exe
+    & $signtoolPath sign /fd SHA512 /tr http://timestamp.comodoca.com /a CitadelGUI\bin\$configuration x86\FilterAgent.Windows.exe
+}
 
 & $msbuildPath /p:Configuration=$configuration /p:SolutionDir=$currentLocation $payload86 /t:Clean,Build
 & $msbuildPath /p:Configuration=$configuration /p:SolutionDir=$currentLocation $setup86 /t:Clean,Build,SignMsi
