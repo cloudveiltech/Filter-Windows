f12f0daa013ae60d1c9a9a7e4a6365ed1629954d
Kent Friesen <kfreezen@gmail.com>
2019-06-27 15:46:58 -0400

    Forgot to change build installer version script.

--- build-installer-version.cmd
+++ build-installer-version.cmd
@@ -2,7 +2,7 @@
 
 If "%1" == "" ( set /p Version=Enter new installer version: ) else ( set Version=%1 )
 
-call update-project-versions.cmd %Version%
+update-project-versions.exe -Version %Version%
 call update-installer-version.cmd %Version%
 
 call build-installer.cmd
