a7cb519cc9dc1fbb39b43387e3fd95c3e0a3a291
Kent Friesen <kfreezen@gmail.com>
2019-06-27 15:45:12 -0400

    Cleanup of assembly version files and new compiled C# script to change them.

--- update-project-versions.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-If "%~1" == "" ( set /p VER="Enter new project version: " ) else ( set VER=%1 )
-
-If "%VER%" == "" ( exit )
-
-set VERSIONSTR=-version %VER%
-
-powershell -ExecutionPolicy Unrestricted -Command .\update-project-versions.ps1 %VERSIONSTR%
--- update-project-versions.ps1
+++
@@ -1,9 +0,0 @@
-param (
-    [string]$version
-)
-
-$arr = Get-ChildItem AssemblyInfo.cs -Recurse
-
-foreach($file in $arr) {
-	((Get-Content -path $file -Raw) -replace '\[assembly:\s(AssemblyVersion|AssemblyFileVersion)\("(.*)"\)\]', ('[assembly: $1("' + $version + '")]')) | Set-Content -Path $file
-}
