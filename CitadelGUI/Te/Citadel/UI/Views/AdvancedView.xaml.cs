ca92aecd6a855091db05719e34b3dec73a55d6ca
Dmitriy Goraschenko <dgoraschenko@gmail.com>
2019-09-25 10:30:44 +0700

    nlog updated to latest version

--- CitadelGUI/Te/Citadel/UI/Views/AdvancedView.xaml.cs
+++ CitadelGUI/Te/Citadel/UI/Views/AdvancedView.xaml.cs
@@ -5,21 +5,10 @@
 * file, You can obtain one at http://mozilla.org/MPL/2.0/.
 */
 using CloudVeil.Windows;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Te.Citadel.UI.ViewModels;
 
 namespace Te.Citadel.UI.Views
