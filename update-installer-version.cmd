933cc05ab334f2d76354bc1ba95ecf6f7bfb3c71
Dmitriy Goraschenko <dgoraschenko@gmail.com>
2024-04-16 17:35:25 +0200

    upgrade to wix4, added arm64 projects

--- update-installer-version.cmd
+++ update-installer-version.cmd
@@ -4,8 +4,10 @@ If "%1" == "" ( set /p InstallerVersion=Enter new installer version: ) else ( se
 
 echo Installer version is %InstallerVersion%
 
+.\wix-verify-bin\wix-verify.exe set Installers\SetupPayloadArm64\Module.wxs wix.module.version %InstallerVersion%
 .\wix-verify-bin\wix-verify.exe set Installers\SetupPayload64\Module.wxs wix.module.version %InstallerVersion%
 .\wix-verify-bin\wix-verify.exe set Installers\SetupPayload86\Module.wxs wix.module.version %InstallerVersion%
+.\wix-verify-bin\wix-verify.exe set Installers\SetupProjects\Product-Arm64.wxs wix.product.version %InstallerVersion%
 .\wix-verify-bin\wix-verify.exe set Installers\SetupProjects\Product-x86.wxs wix.product.version %InstallerVersion%
 .\wix-verify-bin\wix-verify.exe set Installers\SetupProjects\Product-x64.wxs wix.product.version %InstallerVersion%
 .\wix-verify-bin\wix-verify.exe set CloudVeilInstaller\Bundle.wxs Wix.Bundle.Version %InstallerVersion%
